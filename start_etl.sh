eda27ec0a70210134363f4d1edacc212c37d4bd1
Richard Wi <18545483+riwim@users.noreply.github.com>
2022-01-27 18:01:24 +0000

    Extract instance name variable

--- start_etl.sh
+++ start_etl.sh
@@ -2,19 +2,33 @@
 
 # DEPRECATED - use orchestrator instead
 
+readonly INSTANCE_NAME='orchestrator'
 source "gcloud-config.sh"
 
 echo "PROJECT ID: $GCP_NAME"
+echo "SERVICEACCOUNT: $SERVICEACCOUNT"
+echo "ELT_MACHINE_TYPE: $ELT_MACHINE_TYPE"
+echo "IMAGE: $IMAGE"
+echo "IMAGE_PROJECT: $IMAGE_PROJECT"
 gcloud config set project $GCP_NAME
 
-gcloud compute instances create orchestartor \
+gcloud compute instances create ${INSTANCE_NAME} \
+    --zone=${ZONE}\
     --machine-type=${ETL_MACHINE_TYPE}\
     --subnet=default --network-tier=PREMIUM \
     --metadata-from-file=startup-script=./configs/beam-and-bigqueryloader-startup.sh \
     --maintenance-policy=MIGRATE --service-account=${SERVICEACCOUNT} \
-    --scopes=https://www.googleapis.com/auth/bigquery,https://www.googleapis.com/auth/pubsub,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_write \
+    --scopes=https://www.googleapis.com/auth/bigquery,\
+https://www.googleapis.com/auth/pubsub,\
+https://www.googleapis.com/auth/compute,\
+https://www.googleapis.com/auth/servicecontrol,\
+https://www.googleapis.com/auth/service.management.readonly,\
+https://www.googleapis.com/auth/logging.write,\
+https://www.googleapis.com/auth/monitoring.write,\
+https://www.googleapis.com/auth/trace.append,\
+https://www.googleapis.com/auth/devstorage.read_write \
     --tags=etl,http-server,https-server --image=${IMAGE} --image-project=${IMAGE_PROJECT} \
-    --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=orchestrator
+    --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=${INSTANCE_NAME}
 
 echo "[info] etl machine started. Wait until beam-enrich and bigquery_load starts dataflow. Cca. 5-10minutes"
 echo "[test] gcloud dataflow jobs list"
