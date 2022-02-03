e825c8255f865fa047d5557f427ef2d05cdbb9b4
Richard Wi <18545483+riwim@users.noreply.github.com>
2022-02-04 00:26:00 +0100

    Format script and turn off wget output

--- templates/beam-and-bigqueryloader-startup.sh
+++ templates/beam-and-bigqueryloader-startup.sh
@@ -6,23 +6,20 @@ project_id="%PROJECTID%"
 region="%REGION%"
 
 sudo apt-get update
-sudo apt-get -y install default-jre
-sudo apt-get -y install unzip
-sudo apt-get -y install less wget
+sudo apt-get -y install default-jre unzip less wget
 
-
-wget https://github.com/snowplow/enrich/releases/download/beam_enrich%2F1.1.1/snowplow_beam_enrich_1.1.0.zip
-wget https://github.com/snowplow/enrich/releases/download/beam_enrich%2F$enrich_version/snowplow_beam_enrich_$enrich_version.zip
+wget -q https://github.com/snowplow/enrich/releases/download/beam_enrich%2F1.1.1/snowplow_beam_enrich_1.1.0.zip
+wget -q https://github.com/snowplow/enrich/releases/download/beam_enrich%2F$enrich_version/snowplow_beam_enrich_$enrich_version.zip
 unzip snowplow_beam_enrich_$enrich_version.zip
 
-wget https://github.com/snowplow-incubator/snowplow-bigquery-loader/releases/download/$bq_version/snowplow_bigquery_loader_$bq_version.zip
+wget -q https://github.com/snowplow-incubator/snowplow-bigquery-loader/releases/download/$bq_version/snowplow_bigquery_loader_$bq_version.zip
 unzip snowplow_bigquery_loader_$bq_version.zip
 
-wget https://github.com/snowplow-incubator/snowplow-bigquery-loader/releases/download/$bq_version/snowplow_bigquery_mutator_$bq_version.zip
+wget -q https://github.com/snowplow-incubator/snowplow-bigquery-loader/releases/download/$bq_version/snowplow_bigquery_mutator_$bq_version.zip
 unzip snowplow_bigquery_mutator_$bq_version.zip
 
-gsutil cp  ${TEMP_BUCKET}/config/iglu_config.json .
-gsutil cp  ${TEMP_BUCKET}/config/bigqueryloader_config.json .
+gsutil cp ${TEMP_BUCKET}/config/iglu_config.json .
+gsutil cp ${TEMP_BUCKET}/config/bigqueryloader_config.json .
 
 #start beam enrich in data flow
 ./beam-enrich-$enrich_version/bin/beam-enrich \
@@ -39,9 +36,19 @@ gsutil cp  ${TEMP_BUCKET}/config/bigqueryloader_config.json .
     --resolver=iglu_config.json
 
 #create tables
-./snowplow-bigquery-mutator-$bq_version/bin/snowplow-bigquery-mutator create --config $(cat bigqueryloader_config.json | base64 -w 0) --resolver $(cat iglu_config.json | base64 -w 0)
+./snowplow-bigquery-mutator-$bq_version/bin/snowplow-bigquery-mutator create \
+  --config $(cat bigqueryloader_config.json | base64 -w 0) \
+  --resolver $(cat iglu_config.json | base64 -w 0)
 
 #listen to new bq types
-./snowplow-bigquery-mutator-$bq_version/bin/snowplow-bigquery-mutator listen --config $(cat bigqueryloader_config.json | base64 -w 0) --resolver $(cat iglu_config.json | base64 -w 0) &
+./snowplow-bigquery-mutator-$bq_version/bin/snowplow-bigquery-mutator listen \
+  --config $(cat bigqueryloader_config.json | base64 -w 0) \
+  --resolver $(cat iglu_config.json | base64 -w 0) &
 
-./snowplow-bigquery-loader-$bq_version/bin/snowplow-bigquery-loader --config=$(cat bigqueryloader_config.json | base64 -w 0) --resolver=$(cat iglu_config.json | base64 -w 0) --runner=DataFlowRunner --project=$project_id --region=$region --gcpTempLocation=${TEMP_BUCKET}/temp-files
+./snowplow-bigquery-loader-$bq_version/bin/snowplow-bigquery-loader \
+  --config=$(cat bigqueryloader_config.json | base64 -w 0) \
+  --resolver=$(cat iglu_config.json | base64 -w 0) \
+  --runner=DataFlowRunner \
+  --project=$project_id \
+  --region=$region \
+  --gcpTempLocation=${TEMP_BUCKET}/temp-files
